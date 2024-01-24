ca9a88573eecf1409d9a68e1c000ffd7cee64503
Thiago_Reis <reist36@gmail.com>
2024-01-24 13:12:52 -0300

    Created: postService and postRepository to get post by id

--- src/main/java/com/thiagoreis/workshopmongo/resources/PostResource.java
+++ src/main/java/com/thiagoreis/workshopmongo/resources/PostResource.java
@@ -1,19 +1,11 @@
 package com.thiagoreis.workshopmongo.resources;
 
-import java.net.URI;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.thiagoreis.workshopmongo.domain.Post;
 import com.thiagoreis.workshopmongo.services.PostService;
@@ -25,14 +17,6 @@ public class PostResource {
 	@Autowired
 	private PostService postService;
 
-	@GetMapping
-	public ResponseEntity<List<Post>> findAll() {
-
-		List<Post> list = postService.findAll();
-
-		return ResponseEntity.ok().body(list);
-	}
-
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> findByID(@PathVariable String id) {
 
@@ -41,31 +25,4 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
-	@PostMapping
-	public ResponseEntity<Void> insert(@RequestBody Post obj) {
-
-		postService.insert(obj);
-
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-
-		return ResponseEntity.created(uri).build();
-	}
-
-	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable String id) {
-
-		postService.delete(id);
-
-		return ResponseEntity.noContent().build();
-	}
-
-	@PutMapping(value = "/{id}")
-	public ResponseEntity<Void> update(@RequestBody Post obj, @PathVariable String id) {
-
-		obj.setId(id);
-
-		obj = postService.update(obj);
-
-		return ResponseEntity.noContent().build();
-	}
 }
--- src/main/java/com/thiagoreis/workshopmongo/services/PostService.java
+++ src/main/java/com/thiagoreis/workshopmongo/services/PostService.java
@@ -1,6 +1,5 @@
 package com.thiagoreis.workshopmongo.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,33 +15,9 @@ public class PostService {
 	@Autowired
 	private PostRepository repository;
 
-	public List<Post> findAll() {
-		return repository.findAll();
-	}
-
 	public Post findById(String id) {
 		Optional<Post> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-	}
-
-	public Post insert(Post obj) {
-		return repository.insert(obj);
-	}
-
-	public void delete(String id) {
-		repository.deleteById(id);
-	}
-
-	public Post update(Post obj) {
-		Post newObj = findById(obj.getId());
-
-		updateData(newObj, obj);
-		return repository.save(newObj);
-	}
-
-	public void updateData(Post newObj, Post obj) {
-		newObj.setTitle(obj.getTitle());
-		newObj.setBody(obj.getBody());
 	}
 
 }
