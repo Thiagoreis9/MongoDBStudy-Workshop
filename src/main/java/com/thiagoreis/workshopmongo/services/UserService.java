f735e14f33f234ce80a6d3f97508581ab3ce6d78
Thiago_Reis <reist36@gmail.com>
2024-01-23 23:32:54 -0300

    Create: updating user

--- src/main/java/com/thiagoreis/workshopmongo/services/UserService.java
+++ src/main/java/com/thiagoreis/workshopmongo/services/UserService.java
@@ -34,9 +34,21 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
+	
+	public void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	};
 	
 	
 }
