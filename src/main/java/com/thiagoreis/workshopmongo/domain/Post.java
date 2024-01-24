0abc282da9c7642be0ca8e69fbcad39bcb35185e
Thiago_Reis <reist36@gmail.com>
2024-01-24 13:25:18 -0300

    Created: commentDTO and comments reference in posts

--- src/main/java/com/thiagoreis/workshopmongo/domain/Post.java
+++ src/main/java/com/thiagoreis/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.thiagoreis.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.thiagoreis.workshopmongo.dto.AuthorDTO;
+import com.thiagoreis.workshopmongo.dto.CommentDTO;
 
 
 @Document
@@ -22,6 +25,8 @@ public class Post implements Serializable{
 	private String body;
 	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -73,6 +78,10 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 	@Override
